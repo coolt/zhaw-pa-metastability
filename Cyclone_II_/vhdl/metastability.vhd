5ce2e9b2f4cf3c869a5b4d492cbbe691ce832c35
Katrin  Baechli <baek@zhaw.ch>
2015-10-14 13:08:18 +0200

    metastability: 2 Version (not working FSM).

--- Cyclone_II_/vhdl/metastability.vhd
+++ Cyclone_II_/vhdl/metastability.vhd
@@ -15,11 +15,16 @@ use IEEE.NUMERIC_STD.ALL;
 
 
 entity metastability is
-port (CLOCK_50 : 	in std_logic;
-      KEY_0 : 		in std_logic;  -- low activ 
-      GPIO_0_0 :  out std_logic
+port (CLOCK_27 : 	in std_logic;
+		CLOCK_50 : 	in std_logic;
+      SW_17 : 		in std_logic;  
+		GPIO_1_8: 	out std_logic;
+      GPIO_0_0 :  out std_logic;		-- state 
+		GPIO_0_1 :  out std_logic;    -- pulse 
+		LEDR_17:		out std_logic
+		
   );
-end metastability;
+end metastability; 
 
 
 ----------------------------------------------------------------------------------
@@ -32,8 +37,14 @@ architecture behavioral of metastability is
 --------------------------------------------
 type actual_state is (s0, s1, s2, s3, s4, s5, s6, s7); 
 		
-signal state: 			actual_state;  
+signal state: 			actual_state  := s0;  
 signal next_state: 	actual_state;
+
+signal cnt: 			integer range 0 to 15 := 0;  
+signal next_cnt: 		integer range 0 to 15 := 0;  
+signal pulse: 			std_logic     := '0';
+signal cnt_reset:		std_logic     := '0';
+
 
 
 begin
@@ -41,70 +52,84 @@ begin
 --------------------------------------------
 -- clocked process
 --------------------------------------------
-	process (CLOCK_50)
+	fsm: process (all)
 	begin
-		if (rising_edge(CLOCK_50)) then
-		  state <= next_state;   			
+		if (SW_17 = '1') then
+			state <= s2;
+		elsif (rising_edge(CLOCK_50)) then
+		  state <= next_state;
 		end if;
 	end process;
+
+
+	counter: process (all)
+	begin
+		if (cnt_reset = '1') then
+			cnt <= 0;
+		elsif(rising_edge(CLOCK_27)) then
+		  cnt <= next_cnt;   			
+		end if;
+	end process;
+	
+	
 
 	
 --------------------------------------------
 -- input process
---------------------------------------------
-	process (state, KEY_0)
-	
-	begin	
+-------------------------------------------- 
+	counter_input: process (all)	
+	begin			
+			next_cnt <= cnt + 1; 
+	end process;
+
+	fsm_input: process (all)		
+	begin 	  
 	  case state is
-		  when s0 =>  
-			  -- key pressed
-			  if(KEY_0 ='0') then
-				--GPIO_0_0 <= '1';
+	  		  when s0 => 
+			  if(pulse ='1') then
 				next_state <= s1;
 			 else
 				next_state <= s0;
 			  end if;  
 			
 		  when s1 =>    
-			 -- key pressed
-			 if(KEY_0 ='0') then
-				next_state <= s0;
-				--GPIO_0_0 <= '0';
+			 if(pulse ='1') then
+				next_state <= s2;  ----------------
 			 else
 				next_state <= s1;
 			 end if;
 
 		 -- no official states
 		 when s2 =>       
-			 if(KEY_0 ='0') then
+			 if(pulse ='1') then
 				next_state <= s3;
 			 else
 				next_state <= s2;
 			 end if;
 	
 		 when s3 =>       
-			 if(KEY_0 ='0') then
+			 if(pulse ='1') then
 				next_state <= s4;
 			 else
 				next_state <= s3;
 			 end if;
 
 		 when s4 =>       
-			 if(KEY_0 ='0') then
+			 if(pulse ='1') then
 				next_state <= s5;
 			 else
 				next_state <= s4;
 			 end if;
 
 		 when s5 =>       
-			 if(KEY_0 ='0') then
+			 if(pulse ='1') then
 				next_state <= s6;
 			 else
 				next_state <= s5;
 			 end if;
 
 		 when s6 =>       
-			 if(KEY_0 ='0') then
+			 if(pulse ='1') then
 				next_state <= s7;
 			 else
 				next_state <= s6;
@@ -113,22 +138,41 @@ begin
 		when s7 =>       		 
 				next_state <= s7;
 		end case;
+		
 	end process;	
 
 	
 --------------------------------------------
 -- output process
 --------------------------------------------
-	process (state)
+
+	decode_cnt_max: process (all)
+	begin
+		if (cnt = 15) then
+			cnt_reset <= '1';
+			pulse <= '1';
+		else 
+			cnt_reset <= '0';
+			pulse <= '0';
+		end if;
+	end process;	
+	
+	
+	
+	fsm_output: process (all)
 	begin
 		-- not allowed state
 		if (state = s2) or (state = s3) or (state = s4) or (state = s5) or (state = s6) or (state = s7) then
 			GPIO_0_0 <= '1';
+			LEDR_17   <= '1';
 		else
 			GPIO_0_0 <= '0';
+			LEDR_17   <= '0';
 		end if;	
 	end process;	
 	
+	GPIO_0_1 <= pulse;
+	GPIO_1_8 <= SW_17;
 	
 end behavioral;
 
